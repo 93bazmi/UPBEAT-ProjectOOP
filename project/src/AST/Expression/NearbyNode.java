0cca14c06d7b494071ddb3b1ad12fb13e1aea66b
bazmi <bambamboom234@gmail.com>
2024-02-07 05:58:31 +0700

    update AST.Expression

--- project/src/AST/Expression/NearbyNode.java
+++ project/src/AST/Expression/NearbyNode.java
@@ -1,4 +1,24 @@
 package AST.Expression;
 
-public class NearbyNode {
+import Game.Direction;
+import Game.GameCommand;
+
+import static AST.Node.*;
+
+public class NearbyNode extends Expr {
+    private final Direction dir;
+
+    public NearbyNode(Direction dir) {
+        this.dir = dir;
+    }
+
+    @Override
+    public long eval(GameCommand gamecmd) {
+        return gamecmd.nearby(dir);
+    }
+
+    @Override
+    public String toString() {
+        return "nearby " + dir;
+    }
 }
--- project/src/AST/Expression/OpponentNode.java
+++ project/src/AST/Expression/OpponentNode.java
@@ -1,4 +1,17 @@
 package AST.Expression;
 
-public class OpponentNode {
+import Game.GameCommand;
+
+import static AST.Node.*;
+
+public class OpponentNode extends Expr {
+    @Override
+    public long eval(GameCommand gamecmd) {
+        return gamecmd.opponent();
+    }
+
+    @Override
+    public String toString() {
+        return "opponent";
+    }
 }
