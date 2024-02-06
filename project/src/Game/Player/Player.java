c1a9dd258c1ce735a76d92a65e7bee7f8aa8773e
bazmi <bambamboom234@gmail.com>
2024-02-07 03:25:45 +0700

    Add GameState

---
+++ project/src/Game/Player/Player.java
@@ -0,0 +1,16 @@
+package Game.Player;
+
+import java.util.Map;
+
+public interface Player {
+
+    String getName();
+    double getBudget();
+    void updateBudget(double money);
+    void moveCityCrew(String direction);
+    int[] getCityCrewLocation();
+    int[] getCityCenterLocation();
+    boolean life();
+    long attack(String direction);
+    Map<String,Long> getIdentifiers();
+}
---
+++ project/src/Game/Region/Region.java
@@ -0,0 +1,12 @@
+package Game.Region;
+
+import Game.Player.*;
+
+public interface Region {
+    Player getOwner();
+    long getDeposit();
+    void updateDeposit(long amount);
+    void updateOwner(Player owner);
+    int getLocation();
+    long getNearby();
+}
