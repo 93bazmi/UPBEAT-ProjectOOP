7f45a7dad49a6ebc41f624363913aeb24e2db552
Aong noppol Nunpiang <nopphol1122@gmail.com>
2024-02-07 00:48:42 +0700

    ทำ interface อันแรก เวอ 0.1

---
+++ Overview.java
@@ -0,0 +1,40 @@
+public class Overview {
+    public static class Configuration{
+
+    }
+    public interface Territory{
+
+    }
+    public interface Region{
+
+    }
+    public interface Player{
+
+    }
+    public interface ConfigutationPlan{
+
+    }
+    public interface GameController{
+
+    }
+    public interface Time{
+
+    }
+    public interface CityCrew{
+
+    }
+    public interface Parser{
+
+    }
+    public interface AST{
+
+    }
+    public interface Action{
+    void move();
+    void invert();
+    void shoot();
+    void collect();
+    void relocate();
+    }
+}
+
