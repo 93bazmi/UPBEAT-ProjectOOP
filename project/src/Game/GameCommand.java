8265f4142696bf67734d2d5845c84f442d4d9d5b
Aong noppol Nunpiang <nopphol1122@gmail.com>
2024-03-03 21:03:15 +0700

    Player and Region Update

--- project/src/Game/GameCommand.java
+++ project/src/Game/GameCommand.java
@@ -1,32 +1,27 @@
 package Game;
 
+import Game.Region.Region;
+
 import java.util.Map;
 
 public interface GameCommand {
-
     boolean attack(Direction dir, long v);
-
     boolean collect(long v);
-
-    void invest(long eval);
-
-    void relocate();
-
+    boolean invest(long eval);
+    boolean relocate();
     long nearby(Direction dir);
-
     boolean move(Direction dir);
-
     long opponent();
-
-    Map<String, Long> getIdentifiers();
     long getRow();
     long getCol();
     long getCityCrew_Row();
-    long getCitiCrew_Col();
+    long getCityCrew_Col();
     long getBudget();
     long getDeposit();
     long getInterest();
     long getMaxDeposit();
     long getRandom();
+    Region getCityCrew();
+    Map<String, Long> getIdentifiers();
 
 }
